8f4ea8774d703be8679bad5ef0d4c30ccba0e404
SamarV-121 <samarvispute121@gmail.com>
2020-07-27 07:15:41 +0000

    ARM64: configs: Disable KNOX NCM
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -762,7 +762,7 @@ static int sock_set_dns_pid(struct sock *sk, char __user *optval, int optlen)
 out:
 	return ret;
 }
-
+#endif
 /* END_OF_KNOX_NPA */
 #endif
 
@@ -1637,6 +1637,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 				}
 			}
 		}
+#endif
 		/* END_OF_KNOX_NPA */
 #endif
 		/*
