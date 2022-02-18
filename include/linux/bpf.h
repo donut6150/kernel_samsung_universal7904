fbd1a90506610a0b15c6f238c48d9cc7b738555a
Josh Poimboeuf <jpoimboe@redhat.com>
2022-02-18 11:49:08 -0800

    speculation: Include unprivileged eBPF status in Spectre v2 mitigation reporting
    commit 44a3918c8245ab10c6c9719dd12e7a8d291980d8 upstream.
    
    With unprivileged eBPF enabled, eIBRS (without retpoline) is vulnerable
    to Spectre v2 BHB-based attacks.
    
    When both are enabled, print a warning message and report it in the
    'spectre_v2' sysfs vulnerabilities file.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    [fllinden@amazon.com: backported to 4.19]
    Signed-off-by: Frank van der Linden <fllinden@amazon.com>
    [bwh: Backported to 4.9: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [pritam: drop x86 change]
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -361,6 +361,10 @@ static inline void bpf_long_memcpy(void *dst, const void *src, u32 size)
 /* verify correctness of eBPF program */
 int bpf_check(struct bpf_prog **fp, union bpf_attr *attr);
 
+static inline bool unprivileged_ebpf_enabled(void)
+{
+	return !sysctl_unprivileged_bpf_disabled;
+}
 struct bpf_prog *bpf_prog_get_type_path(const char *name, enum bpf_prog_type type);
 
 #else
@@ -389,6 +393,11 @@ static inline void bpf_prog_put(struct bpf_prog *prog)
 static inline struct bpf_prog *bpf_prog_inc(struct bpf_prog *prog)
 {
 	return ERR_PTR(-EOPNOTSUPP);
+}
+
+static inline bool unprivileged_ebpf_enabled(void)
+{
+	return false;
 }
 
 static inline int __bpf_prog_charge(struct user_struct *user, u32 pages)
